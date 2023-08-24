5fca2176d8f50097e7a701a7020f22038924ae11
g5becks <techstar.dev@hotmail.com>
2023-08-23 20:58:27 -0400

    feat(Collection): Collection type
    add collection  type

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module structbox
 
 go 1.21
+
+require go.etcd.io/bbolt v1.3.7
+
+require golang.org/x/sys v0.4.0 // indirect
--- structbox.go
+++ structbox.go
@@ -1,1 +1,45 @@
 package structbox
+
+import (
+	"fmt"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+type StructBox struct {
+	db *bolt.DB
+}
+
+func New(db *bolt.DB) *StructBox {
+	return &StructBox{db: db}
+}
+
+type Collection[T any] struct {
+	Name string
+	sb   *StructBox
+}
+
+func bucketName[T interface{}]() []byte {
+	var ref T
+	return []byte(fmt.Sprintf("structbox:%T", &ref))
+}
+
+var (
+	ErrCollectionNotFound = fmt.Errorf("collection not found")
+	ErrCreatingCollection = fmt.Errorf("error creating collection")
+)
+
+func NewCollection[T interface{}](sb *StructBox) (*Collection[T], error) {
+	path := bucketName[T]()
+	err := sb.db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(path)
+		if err != nil {
+			return fmt.Errorf("%w: %w", ErrCreatingCollection, err)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", ErrCreatingCollection, err)
+	}
+	return &Collection[T]{sb: sb, Name: string(path)}, nil
+}
